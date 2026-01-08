187cc8c248161c36b1e31bc60c6929134d7bcd99
whausleitner <58576046+whausleitner@users.noreply.github.com>
2026-01-08 09:10:35 +0100

    Update foo.c

--- foo.c
+++ foo.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-    printf("%s\n", "Hallo Welt!");
+printf("Aufgabe 1: Weihnachtskekse");
+int number_buscuits = 0;
+float diameter_buscuit = 0;
+float area = 0;
+
+    printf("Geben sie die Anzahl der Kekse ein: ");
+    scanf("%d", &number_buscuits);
+    printf("Geben sie den Durchmesser eines Keks ein: ");
+    scanf("%f", &diameter_buscuit);
+
+    area = pow((diameter_buscuit/2),2)*3*number_buscuits;
+
+    printf("Für die Glasur aller %d Kekse wird Schokolade für %f cm² benötigt.", number_buscuits, area);
+
+    return 0;
 }
